a6d9b76c789167c31bd0d770f39757d61a2f4395
luoxwen <luoxwen@gmail.com>
2016-09-01 20:49:10 +0800

    Change picture in section 1.

--- js/index.js
+++ js/index.js
@@ -1,48 +1,50 @@
 var availableLanguage = [
-  ['zh', '中文-简体'],
-  ['en', 'English'],
+    ['zh', '中文-简体'],
+    ['en', 'English'],
 ];
 
 $(document).ready(function() {
 
-  var lang = getCookie('language');
-  if (lang === null) {
-    lang = detectLanguage();
-  }
-
-  changeText(lang);
-
-  var handleclick = function() {
-    var index = $('#languageList').children().index(this);
-    var item = availableLanguage[index];
-    changeLanguage(index);
-    $('#currentLang').text(item[1]);
-
-    $('#languageList').children('li').removeClass('active');
-    $(this).addClass('active');
-};
-
-  for (var i = 0; i < availableLanguage.length; i++) {
-    var it = availableLanguage[i];
-    var dom = $('<li><a href="#">' + it[1] + '</a></li>');
-    dom.click(handleclick).appendTo('#languageList');
-    if (lang == it[0]) {
-      dom.addClass('active');
-      $('#currentLang').text(it[1]);
+    var lang = getCookie('language');
+    if (lang === null) {
+        lang = detectLanguage();
     }
-  }
-  $('#changeLanguage').click(function() {
-    var list = $('#languageList');
-    if (list.is(':visible')) {
-      list.slideUp();
-      $('#changeLanguage .triangle').removeClass('reverse');
-    } else {
-      list.slideDown();
-      $('#changeLanguage .triangle').addClass('reverse');
+
+    changeText(lang);
+
+    var handleclick = function() {
+        var index = $('#languageList').children().index(this);
+        var item = availableLanguage[index];
+        changeLanguage(index);
+        $('#currentLang').text(item[1]);
+
+        $('#languageList').children('li').removeClass('active');
+        $(this).addClass('active');
+    };
+
+    for (var i = 0; i < availableLanguage.length; i++) {
+        var it = availableLanguage[i];
+        var dom = $('<li><a href="#">' + it[1] + '</a></li>');
+        dom.click(handleclick).appendTo('#languageList');
+        if (lang == it[0]) {
+            dom.addClass('active');
+            $('#currentLang').text(it[1]);
+        }
     }
-  });
-  $('#introduce').click(smoothScroll);
-  $('#about').click(smoothScroll);
+    $('#changeLanguage').click(function() {
+        var list = $('#languageList');
+        if (list.is(':visible')) {
+            $(this).removeClass('open');
+            list.slideUp();
+            $('#changeLanguage .triangle').removeClass('reverse');
+        } else {
+            $(this).addClass('open');
+            list.slideDown();
+            $('#changeLanguage .triangle').addClass('reverse');
+        }
+    });
+    $('#introduce').click(smoothScroll);
+    $('#about').click(smoothScroll);
 });
 
 function smoothScroll() {
@@ -53,45 +55,45 @@ function smoothScroll() {
 }
 
 function changeText(lang) {
-  $.i18n.properties({
-    name: 'strings',
-    path: 'i18n/',
-    mode: 'map',
-    language: lang,
-    callback: function() {
-      for (var key in $.i18n.map) {
-        $('#' + key).html($.i18n.prop(key));
-      }
-    }
-  });
+    $.i18n.properties({
+        name: 'strings',
+        path: 'i18n/',
+        mode: 'map',
+        language: lang,
+        callback: function() {
+            for (var key in $.i18n.map) {
+                $('#' + key).html($.i18n.prop(key));
+            }
+        }
+    });
 }
 
 function changeLanguage(index) {
-  setCookie('language', availableLanguage[index][0]);
-  changeText(availableLanguage[index][0]);
+    setCookie('language', availableLanguage[index][0]);
+    changeText(availableLanguage[index][0]);
 }
 
 function detectLanguage() {
-  var detectedLng = (navigator.language) ? navigator.language : navigator.userLanguage;
-  if (detectedLng.indexOf('zh') >= 0) {
-    return 'zh';
-  } else {
-    return 'en';
-  }
+    var detectedLng = (navigator.language) ? navigator.language : navigator.userLanguage;
+    if (detectedLng.indexOf('zh') >= 0) {
+        return 'zh';
+    } else {
+        return 'en';
+    }
 }
 
 function setCookie(name, value) {
-  var cookie = name + '=' + escape(value);
-  document.cookie = cookie;
+    var cookie = name + '=' + escape(value);
+    document.cookie = cookie;
 }
 
 function getCookie(name) {
-  var arr;
-  var reg = new RegExp('(^| )' + name + '=([^;]*)(;|$)');
+    var arr;
+    var reg = new RegExp('(^| )' + name + '=([^;]*)(;|$)');
 
-  if (arr = document.cookie.match(reg)) {
-    return unescape(arr[2]);
-  } else {
-    return null;
-  }
+    if (arr = document.cookie.match(reg)) {
+        return unescape(arr[2]);
+    } else {
+        return null;
+    }
 }
